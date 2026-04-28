6c850203199b9407c8aa268ca38fc36be585cc8b
denism96 <drag33546@bk.ru>
2026-04-28 21:46:02 +0300

    styled choice component

--- client/components/Home/Choice.jsx
+++ client/components/Home/Choice.jsx
@@ -15,7 +15,23 @@ export default function Choice() {
       </section>
       <section>
         <div className="flex items-center justify-between">
-          <button>---</button>
+          <button className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[var(--bg-red-color)] text-white opacity-60">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </button>
+
           <div className="flex w-[985px] flex-col items-center justify-between">
             <h2 className="text-2xl font-semibold text-[var(--red-text)]">
               WHAT OUR CUSTOMERS SAY
@@ -30,10 +46,77 @@ export default function Choice() {
               <span className="text-2xl opacity-60">Aleea Thompson</span>
             </div>
           </div>
-          <button>---</button>
+
+          <button className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[var(--bg-red-color)] text-white opacity-60">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </button>
         </div>
       </section>
-      <section></section>
+      <section className="mt-[100px]">
+        <div className="flex flex-col items-center justify-center bg-[url(./images/others/drivoxe_bg.svg)] bg-contain bg-no-repeat">
+          <h3 className="text-xl">HOW IT WORKS</h3>
+          <h2 className="text-5xl">Simple Steps to Get the Car</h2>
+          <div className="mt-[100px] grid w-2/3 grid-cols-2 gap-4">
+            <div className="col-span-2 flex">
+              <div className="mr-8 w-[156px]">
+                <h3 className="text-right text-2xl">Select</h3>
+                <p className="text-right text-lg">
+                  Choose your desired car from our fleet.
+                </p>
+              </div>
+              <div className="flex h-[120px] w-[120px] items-center justify-center rounded-[30px] bg-white">
+                <img src="./images/icons/touch.svg" alt="touch_icon" />
+              </div>
+            </div>
+            <div className="col-span-2 col-start-2 flex">
+              <div className="flex h-[120px] w-[120px] items-center justify-center rounded-[30px] bg-red-300">
+                <img src="./images/icons/book.svg" alt="book_icon" />
+              </div>
+              <div className="ml-8 w-[156px]">
+                <h3 className="text-2xl">Book</h3>
+                <p className="text-lg">
+                  Reserve your car online or through our app.
+                </p>
+              </div>
+            </div>
+            <div className="col-span-2 flex">
+              <div className="mr-8 w-[156px]">
+                <h3 className="text-right text-2xl">Drive</h3>
+                <p className="text-right text-lg">
+                  Pick up your car and hit the road.
+                </p>
+              </div>
+              <div className="flex h-[120px] w-[120px] items-center justify-center rounded-[30px] bg-white">
+                <img src="./images/icons/drive.svg" alt="drive_icon" />
+              </div>
+            </div>
+            <div className="col-span-2 col-start-2 flex">
+              <div className="flex h-[120px] w-[120px] items-center justify-center rounded-[30px] bg-white">
+                <img src="./images/icons/car.svg" alt="car_icon" />
+              </div>
+              <div className="ml-8 w-[156px]">
+                <h3 className="text-2xl">Return</h3>
+                <p className="text-lg">
+                  Bring the car back at the end of your rental period.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
