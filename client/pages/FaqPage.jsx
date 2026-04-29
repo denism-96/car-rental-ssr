a7cb79ab4ac6089c427c30f70ff2dbdac05f6d0a
denism96 <drag33546@bk.ru>
2026-04-29 22:39:34 +0300

    Create FaqPage.jsx

---
+++ client/pages/FaqPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function FaqPage() {
+  return (
+    <section className="mt-32">
+      <div className="flex flex-col items-center">
+        <h2 className="mb-6 text-xl">Home/FAQs</h2>
+        <h1 className="mb-14 text-5xl font-bold">Frequently Asked Questions</h1>
+        <img
+          className="rounded-3xl"
+          src="/images/others/touch_panel.png"
+          alt="touch car panel"
+        />
+      </div>
+    </section>
+  );
+}
