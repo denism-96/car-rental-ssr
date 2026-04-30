2f6f3e04a2b90c7f77c4bbc8f2e86fa1eadc1f9f
denism96 <drag33546@bk.ru>
2026-04-30 23:45:40 +0300

    add faqpage banner

--- client/pages/FaqPage.jsx
+++ client/pages/FaqPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Accordeon from "../components/Ui/Accordeon";
+import Banner from "../components/Ui/Banner.jsx";
+import Button from "../components/Ui/Button.jsx";
 import accordeonData from "../../server/data/accordeonsData.js";
-
 export default function FaqPage() {
   return (
     <section className="mt-32">
@@ -15,10 +16,21 @@ export default function FaqPage() {
         />
       </div>
       <section className="grid grid-cols-2">
-        {accordeonData.map((item, index) => {
-          return <Accordeon accData={item} key={index} title={item[0].title} />;
-        })}
+        {accordeonData.map((item, index) => (
+          <Accordeon accData={item} key={index} title={item[0].title} />
+        ))}
       </section>
+      <Banner
+        className={
+          "mb-10 flex items-center bg-[url(./images/banners/faq_banner.png)]"
+        }
+      >
+        <div className="ml-24 w-[360px]">
+          <h2 className="text-5xl">Still Have Questions?</h2>
+          <p className="my-6 text-xl font-medium">Contact Us for Assistance</p>
+          <Button className={"btn btn_red"}>Contact Us</Button>
+        </div>
+      </Banner>
     </section>
   );
 }
