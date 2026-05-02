e6a58c36f327b535e5d9e388526897dad6f5480f
denism96 <drag33546@bk.ru>
2026-05-02 22:13:55 +0300

    Update FaqPage.jsx

--- client/pages/FaqPage.jsx
+++ client/pages/FaqPage.jsx
@@ -1,35 +1,28 @@
 import React from "react";
-import Accordeon from "../components/Ui/Accordeon";
 import Banner from "../components/Ui/Banner.jsx";
 import Button from "../components/Ui/Button.jsx";
-import accordeonData from "../../server/data/accordeonsData.js";
+import AccordeonList from "../components/Ui/Accordeon/AccordeonList.jsx";
+import TitlePages from "../components/TitlePages.jsx";
 export default function FaqPage() {
   return (
     <section className="mt-32">
-      <div className="flex flex-col items-center">
-        <h2 className="mb-6 text-xl">Home/FAQs</h2>
-        <h1 className="mb-14 text-5xl font-bold">Frequently Asked Questions</h1>
-        <img
-          className="rounded-3xl"
-          src="/images/others/touch_panel.png"
-          alt="touch car panel"
-        />
-      </div>
+      <TitlePages
+        pathPage={"Home/FAQs"}
+        title={"Frequently Asked"}
+        url={"/images/others/touch_panel.png"}
+      />
       <section className="grid grid-cols-2">
-        {accordeonData.map((item, index) => (
-          <Accordeon accData={item} key={index} title={item[0].title} />
-        ))}
+        <AccordeonList />
       </section>
       <Banner
-        className={
-          "mb-10 flex items-center bg-[url(./images/banners/faq_banner.png)]"
-        }
+        title={"Still Have Questions?"}
+        url={"./images/banners/faq_banner.png"}
+        isButton={true}
+        textButton={"Contact Us"}
       >
-        <div className="ml-24 w-[360px]">
-          <h2 className="text-5xl">Still Have Questions?</h2>
-          <p className="my-6 text-xl font-medium">Contact Us for Assistance</p>
-          <Button className={"btn btn_red"}>Contact Us</Button>
-        </div>
+        <p className="my-6 text-xl font-medium uppercase">
+          Contact Us for Assistance
+        </p>
       </Banner>
     </section>
   );
